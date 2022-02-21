f798cd4a0cc951f3f1c7468d8524f790c31f9718
KnightWorker37 <knightworker37@gmail.com>
2022-02-21 08:54:56 +0300

    Adding image + style to the Header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Header.css';
+import trollFace from './troll-face.png';
 
 const Header = () => (
-  <div>Meme Generator</div>
+  <header className="header">
+    <img src={trollFace} className="header--image" alt="logo" />
+    <h2 className="header--title">Meme Generator</h2>
+    <h4 className="header--project">React Course - Project 3</h4>
+  </header>
 );
 
 export default Header;
