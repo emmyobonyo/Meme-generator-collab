ae8d93a03eb89161336f0895b56098bdb8ab2a65
KnightWorker37 <knightworker37@gmail.com>
2022-02-21 09:47:51 +0300

    Addng test for function

---
+++ src/form/api.test.js
@@ -0,0 +1,33 @@
+import getMemes from './api';
+
+test('fetch memes', () => {
+  jest.spyOn(global, 'fetch').mockImplementation((url) =>
+    Promise.resolve({
+      success: true,
+      data: {
+        memes: [
+          {
+            id: '181913649',
+            name: 'Drake Hotline Bling',
+            url: 'https://i.imgflip.com/30b1gx.jpg',
+            width: 1200,
+            height: 1200,
+            box_count: 2,
+          },
+          {
+            id: '87743020',
+            name: 'Two Buttons',
+            url: 'https://i.imgflip.com/1g8my4.jpg',
+            width: 600,
+            height: 908,
+            box_count: 3,
+          },
+        ],
+      },
+    })
+  );
+
+  expect(getMemes()).resolves.toHaveProperty('success', true);
+  expect(getMemes()).resolves.toHaveProperty('data.memes.0.id', '181913649');
+  expect(getMemes()).resolves.toHaveProperty('data.memes.0.url', 'https://i.imgflip.com/30b1gx.jpg');
+});
