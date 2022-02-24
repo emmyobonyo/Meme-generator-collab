f2c68bbb0c7fafdf9cf9b3d3e8050191a91b6b10
KnightWorker37 <knightworker37@gmail.com>
2022-02-24 14:55:04 +0300

    Fixing ES lint

--- src/form/api.js
+++ src/form/api.js
@@ -1,6 +1,5 @@
-const getMemes = () =>
-  fetch(process.env.REACT_APP_MEME_API_URL)
-    .then((response) => response.json())
-    .then((body) => body.data.memes);
+const getMemes = () => fetch(process.env.REACT_APP_MEME_API_URL)
+  .then((response) => response.json())
+  .then((body) => body.data.memes);
 
 export default getMemes;
