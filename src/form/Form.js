39272adfc43917641fde8505df6fda85fa9b8a91
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-04 14:31:10 +0300

    Display image on canvas

--- src/form/Form.js
+++ src/form/Form.js
@@ -3,6 +3,18 @@ import React, { useState } from 'react';
 import url from './api';
 
 function Meme() {
+  window.onload = () => {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+    img.onload = function () {
+      context.drawImage(img, 0, 0, window.innerWidth, window.innerHeight);
+    };
+    img.src = 'http://i.imgflip.com/1bij.jpg';
+  };
+
   const [imageURL, setImageUrl] = useState({
     image: 'http://i.imgflip.com/1bij.jpg',
     topText: '',
@@ -26,7 +38,8 @@ function Meme() {
         <button className="form-button" type="button" onClick={changeImage}>Get a new Image &#128512;</button>
       </div>
       <div className="image">
-        <img className="api-image" src={imageURL.image} alt="url" />
+        {/* <img className="api-image" src={imageURL.image} alt="url" /> */}
+        <canvas id="canvas" width="300px" />
         <div className="Top-Text">{imageURL.topText}</div>
         <div className="Bottom-Text">{imageURL.bottomText}</div>
       </div>
