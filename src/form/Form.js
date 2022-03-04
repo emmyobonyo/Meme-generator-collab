ef2f9709503c316e0f0b4ff9ce72bf5fbc746808
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-04 18:18:00 +0300

    Center Text on canvas

--- src/form/Form.js
+++ src/form/Form.js
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 import url from './api';
 
 function Meme() {
+  const [imageURL, setImageUrl] = useState({
+    image: 'http://i.imgflip.com/1bij.jpg',
+    // topText: '',
+    // bottomText: '',
+  });
+
   window.onload = () => {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
@@ -12,23 +18,28 @@ function Meme() {
       context.canvas.height = img.height;
       context.drawImage(img, 0, 0, img.width, img.height);
     };
-    img.src = 'http://i.imgflip.com/1bij.jpg';
+    img.src = imageURL.image;
   };
 
-  const [imageURL, setImageUrl] = useState({
-    image: 'http://i.imgflip.com/1bij.jpg',
-    topText: '',
-    bottomText: '',
-  });
   const changeImage = () => {
     const topText = document.getElementById('top-text');
     const bottomText = document.getElementById('bottom-text');
     const number = Math.floor(Math.random() * 101);
     setImageUrl({
       image: url[number].url,
-      topText: topText.value,
-      bottomText: bottomText.value,
     });
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.onload = function () {
+      context.canvas.width = img.width;
+      context.canvas.height = img.height;
+      context.drawImage(img, 0, 0, img.width, img.height);
+      context.font = '40px san-serif';
+      context.fillText(topText.value, 280, 50);
+      context.fillText(bottomText.value, 280, 300);
+    };
+    img.src = imageURL.image;
   };
   return (
     <main>
@@ -40,8 +51,8 @@ function Meme() {
       <div className="image">
         {/* <img className="api-image" src={imageURL.image} alt="url" /> */}
         <canvas id="canvas" width="300px" />
-        <div className="Top-Text">{imageURL.topText}</div>
-        <div className="Bottom-Text">{imageURL.bottomText}</div>
+        {/* <div className="Top-Text">{imageURL.topText}</div> */}
+        {/* <div className="Bottom-Text">{imageURL.bottomText}</div> */}
       </div>
     </main>
   );
